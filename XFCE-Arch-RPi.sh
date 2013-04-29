feae9e07505a66ff521189d38df4130a7a46d6f7
Aditya Patawari <aditya@adityapatawari.com>
2013-04-29 13:04:11 +0530

    adding curl urls to fetch xinitrc and bash_profile

--- XFCE-Arch-RPi.sh
+++ XFCE-Arch-RPi.sh
@@ -10,7 +10,7 @@ pacman -S --noconfirm slim
 systemctl enable slim.service
 systemctl enable graphical.target
 
-curl >> ~/.xinitrc
-curl >> ~/.bash_profile
+curl https://raw.github.com/adimania/arch-desktop-environments/master/xinitrc >> ~/.xinitrc
+curl https://raw.github.com/adimania/arch-desktop-environments/master/bash_profile >> ~/.bash_profile
 
 reboot
